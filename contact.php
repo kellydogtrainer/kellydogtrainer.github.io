7b43b10f926d5ee326e718359faef3e32c65a9c0
kellydogtrainer <58891852+kellydogtrainer@users.noreply.github.com>
2019-12-18 22:36:17 +0000

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,5 +1,23 @@
-﻿<?php
-if($_POST["message"]) {
-    mail("kellydogtrainer@gmail.com", "Form to email message", $_POST["message"], "From: ["email"]");
-}
+<?php
+       // from the form
+       $name = trim(strip_tags($_POST['fname']));
+       $email = trim(strip_tags($_POST['email']));
+       $message = htmlentities($_POST['message']);
+
+       // set here
+       $subject = "Contact form submitted!";
+       $to = 'kellydogtrainer@email.com';
+
+       $body = <<<HTML
+$message
+HTML;
+
+       $headers = "From: $email\r\n";
+       $headers .= "Content-type: text/html\r\n";
+
+       // send the email
+       mail($to, $subject, $body, $headers);
+
+       // redirect afterwords, if needed
+       header('Location: index.html');
 ?>
